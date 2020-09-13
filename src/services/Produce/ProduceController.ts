2ae6ebf481b6a951e383356225daefe4b74f942f
Liam Wright <mail@liam-wright.co.uk>
2020-09-13 15:56:03 +0100

    All endpoints functional. TODO: Error handling of bad data Meaningful return codes Ensure Bulk upload async tasks don't overlap

--- src/services/Produce/ProduceController.ts
+++ src/services/Produce/ProduceController.ts
@@ -1,32 +1,32 @@
-import {ProductModel} from '../../models/Product'
+import {IProduct, ProductModel} from '../../models/Product'
 
-export const getAllProduceItems = async(req, res) =>
-{
+export const getAllProduceItems = async(req, res) => {
     try {
-        await ProductModel.find({},(err, result) =>
-        {
+        await ProductModel.find({},(err, result) => {
             if(err) {
                 res.send(err);
+                return;
             }
             res.json(result);
+            return;
         }).sort({updated: -1});
     }
     catch (ex) {
         return ex;
     }
 }
-export const getProductByName = async(req, res) =>
-{
+export const getProductByName = async(req, res) => {
     if(req?.params?.productName) {
         const productName = req.params.productName;
 
         try {
-            await ProductModel.find({name: productName},(err, result) =>
-            {
+            await ProductModel.find({name: productName},(err, result) => {
                 if(err) {
                     res.send(err);
+                    return;
                 }
                 res.json(result);
+                return;
             });
         }
         catch (ex) {
@@ -34,18 +34,45 @@ export const getProductByName = async(req, res) =>
         }
     }
 }
-export const updatePriceForProduct = async(req, res) =>
-{
+export const updatePriceForProduct = async(req, res) => {
     if(req?.params?.productName) {
         const productName = req.params.productName;
 
         try {
-            await ProductModel.findOneAndUpdate({name: productName}, req.body, {new: true},(err, result) =>
-            {
+            await ProductModel.findOneAndUpdate({name: productName}, req.body, {new: true},(err, result) => {
+                if(err) {
+                    res.send(err);
+                    return;
+                }
+                res.json(result);
+                return;
+            });
+        }
+        catch (ex) {
+            return ex;
+        }
+    }
+}
+export const uploadNewProduceList = async(req, res) => {
+    if(req.body) {
+        
+        try {
+            const productArray = req.body;
+
+            await ProductModel.deleteMany((err, result) => {
+                if(err) {
+                    res.send(err);
+                }
+
+            });
+            //Validation TODO
+            await ProductModel.insertMany(productArray,(err, result) => {
                 if(err) {
                     res.send(err);
                 }
                 res.json(result);
+                return;
+            
             });
         }
         catch (ex) {
--- src/services/Produce/Routes.ts
+++ src/services/Produce/Routes.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { getAllProduceItems, getProductByName, updatePriceForProduct } from "./ProduceController";
+import { getAllProduceItems, getProductByName, updatePriceForProduct, uploadNewProduceList } from "./ProduceController";
 
 const address = "/api/v1"
 export default [
@@ -25,35 +25,31 @@ export default [
           
         const result = await getProductByName(req,res);
         res.status(200).send(result);
-      
-        res.status(400).send("Invalid or empty search term");
-  
 
         }
 
     ]
   },
- /* {
+ {
      //Bulk Upload
     path: address + "/produce",
     method: "post",
     handler: [
         async (req: any, res: Response) => {
-            //const result = await getAllProduceItems();
+          if(req.body) {
+            const result = await uploadNewProduceList(req, res);
             res.status(200).send(result);
-
+          }
         }
-
     ]
-  }, */
+  },
   {
     //Update Price
     path: address + "/produce/:productName",
     method: "put",
     handler: [
         async (req: any, res: Response) => {
-          if(req.body)
-          {
+          if(req.body) {
             const result = await updatePriceForProduct(req, res);
             res.status(200).send(result);
           }
